e54180de4cf4ee518fbf8ab393b9a903e240eee9
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-10-19 18:27:45 +0530

    complete new message component

--- client/src/components/NewMessage.js
+++ client/src/components/NewMessage.js
@@ -10,23 +10,37 @@ function NewMessage() {
         e.preventDefault();
 
         // Replace with your actual API endpoint
+        const messageData = {
+            text: text,
+            user: user,
+            added: new Date(),
+        };
+
         fetch('/api/messages', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ text, user }),
+            body: JSON.stringify(messageData),
         })
             .then((response) => response.json())
             .then(() => {
                 // Optionally, you can update the message list here to reflect the new message.
+                setText(''); // Clear the text field
+                setUser(''); // Clear the user field
             })
             .catch((error) => console.error('Error:', error));
     };
 
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter' && e.shiftKey === false) {
+            e.preventDefault();
+            handleSubmit(e);
+        }
+    };
+
     return (
-        <div>
-            {/* <Typography variant="h4">Add New Message</Typography> */}
+        <div style={{marginBottom:'20px'}}>
             <form onSubmit={handleSubmit}>
                 <div style={{ display: 'flex' }}>
                     <div style={{ display: 'flex', flexDirection: 'column', flex: '1' }}>
@@ -50,11 +64,10 @@ function NewMessage() {
                                 onChange={(e) => setText(e.target.value)}
                                 fullWidth // Expand to full width
                                 size="small" // Decrease the height
-                                sx={{ mb: 2 }}
                             />
                         </div>
                     </div>
-                    <div style={{ alignSelf: 'flex-end', marginLeft: "10px", marginBottom: '17px' }}>
+                    <div style={{ alignSelf: 'flex-end', marginLeft: '10px', marginBottom: '17px' }}>
                         <Button type="submit" variant="contained" color="primary" sx={{ height: '40px' }}>
                             <SendIcon />
                         </Button>
