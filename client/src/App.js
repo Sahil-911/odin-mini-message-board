01befd72b6a996cd616589136d72c58e17215ce0
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-10-19 18:02:09 +0530

    route App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import MessageList from "./components/MessageList";
+import NewMessage from "./components/NewMessage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MessageList />} />
+          <Route path="/new" element={<NewMessage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
